828e9cca12c3630eb9eef2c5e6c3ad3feaa05379
macmule <bentoms@btopenworld.com>
2013-11-14 21:50:51 +0000

    Update EAAdobeUnitTypesVersion.sh

--- EAAdobeUnitTypesVersion.sh
+++ EAAdobeUnitTypesVersion.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+####################################################################################################
+#
+# More information: http://macmule.com/2011/03/09/installing-adobe-unit-types-2-1-0/
+#
+# GitRepo: https://github.com/macmule/AdobeUnitTypesVersion/
+#
+# License: http://macmule.com/license/
+#
+####################################################################################################
 
 #Check to see if Adobe Unit Types is installed
 if [ -d /Library/ScriptingAdditions/Adobe\ Unit\ Types.osax ]; then
